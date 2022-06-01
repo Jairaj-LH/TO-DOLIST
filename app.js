edde0170330e0d45687f94353aa30cf88d00003f
Vadiraj Karanam <vadirajraokaranam@gmail.com>
2022-06-01 20:31:09 +0530

    make it better

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const users = require('./controllers/users')
 const flash = require('connect-flash')
 const db_url = process.env.DB_URL||'mongodb://localhost:27017/todo-list'
 // ||'mongodb://localhost:27017/todo-list'
-mongoose.connect(db_url, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(db_url, { useNewUrlParser: true, useUnifiedTopology: true  })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error!'))
 db.once("open", () => {
